42be7e8a566c7ff2b403b0c0d1143fa8eb1dfa0b
neec <neec@localhost.localdomain>
2020-06-29 15:02:17 +0900

    2nd commit

---
+++ sql/sgrpg2.sql
@@ -0,0 +1,25 @@
+/*---------------------------*/
+/* データベースを新規作成         */
+/*---------------------------*/
+CREATE DATABASE IF NOT EXISTS sgrpg;
+USE sgrpg;
+
+/*---------------------------*/
+/* テーブルを作成               */
+/*---------------------------*/
+-- キャラクター マスター
+CREATE TABLE Chara(
+    id   integer AUTO_INCREMENT,
+    name varchar(64),
+
+    PRIMARY KEY(id)
+);
+
+-- ユーザーが所持しているキャラクター
+CREATE TABLE UserChara(
+    id       integer AUTO_INCREMENT,
+    user_id  integer,
+    chara_id integer,
+
+    PRIMARY KEY(id)
+);
